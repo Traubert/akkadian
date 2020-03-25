854f88365d0f2373f74c0900f27b95257ff6947a
Sam Hardwick <sam.hardwick@iki.fi>
2020-03-25 09:47:57 +0200

    Switching computers sync

--- scripts/autosplit_sentences.py
+++ scripts/autosplit_sentences.py
@@ -2,8 +2,14 @@ import sys, os
 
 columns_comment = "# global.columns = ID FORM LEMMA UPOS XPOS FEATS HEAD DEPREL DEPS MISC\n"
 
+conservative = False
+
+logfilename = "/dev/null"
 readdirpath = sys.argv[1]
 writedirpath = sys.argv[2]
+if len(sys.argv) > 3:
+    logfilename = sys.argv[3]
+log_fobj = open(logfilename, "w")
 
 def split(l, val):
     retval = []
@@ -27,15 +33,28 @@ def get_id(line):
     return line.split('\t')[0]
 
 def convert_file(name, read_fobj, write_fobj):
-    write_fobj.write(columns_comment)
+    log_fobj.write("Converting {}\n".format(name))
+#    write_fobj.write(columns_comment)
     sent_num = 1
-    lines = list(filter(not_comment, read_fobj.readlines()))
+#    lines = list(filter(not_comment, read_fobj.readlines()))
+    lines = read_fobj.readlines()
     sentences = split(lines, "\n")
     for sentence in sentences:
+        log_fobj.write("Reading sentence {}".format(sentence[0]))
         head_ids = set()
         parent2children = {}
         heads = []
-        for line in sentence:
+        splits = []
+
+        def collect_children(root):
+            collection = []
+            for child in parent2children[root]:
+                collection.append(child)
+                if child in parent2children:
+                    collection += collect_children(child)
+            return collection
+        
+        for line in filter(not_comment, sentence):
             head = get_head(line)
             this_id = get_id(line)
             head_ids.add(head)
@@ -44,17 +63,86 @@ def convert_file(name, read_fobj, write_fobj):
             else:
                 parent2children[head].append(this_id)
         for i, line in enumerate(sentence):
+            if not not_comment(line):
+                continue
             if get_id(line) in head_ids and get_head(line) == '_':
                 parts = line.split('\t')
                 parts[6] = '0'
                 parts[7] = 'root'
                 parts[8] = '0:root'
                 heads.append(get_id(line))
-            sentence[i] = parts.join('\t')
+                sentence[i] = '\t'.join(parts)
+        lastright = None
+        for head in heads:
+            children = list(map(int, collect_children(head)))
+            leftmost = min(children + [int(head)])
+            if lastright != None:
+                if not conservative or leftmost == lastright + 1:
+                    log_fobj.write("Inserting split at original id {}\n".format(lastright))
+                    splits.append(lastright)
+            lastright = max(children + [int(head)])
+        write_fobj.write(apply_splits(list(map(lambda x: x.strip('\n'), sentence)), splits))
+    log_fobj.write("\n")
+#            print(leftmost, rightmost)
             
-            
-    print(sentences)
+
+#    print(sentences)
         
+def apply_splits(lines, splits):
+    new_lines = []
+    split = 0
+    offset = 0
+    for line in lines:
+        if line.startswith('#'):
+            new_lines.append(line)
+            continue
+        parts = line.split('\t')
+        if len(parts) < 10:
+            new_lines.append(line)
+            continue
+        if '-' in parts[0]:
+            this_word_num = int(parts[0][:parts[0].index('-')])
+        else:
+            this_word_num = int(parts[0])
+        if len(splits) > split and this_word_num > splits[split]:
+            offset = splits[split]
+            split += 1
+            new_lines.append('')
+        if '-' in parts[0]:
+            start, stop = parts[0].split('-')
+            part_0 = '{}-{}'.format(int(start) - offset, int(stop) - offset)
+        else:
+            part_0 = str(int(parts[0]) - offset)
+        part_6 = parts[6]
+        if part_6 != '_' and part_6 != '0':
+            part_6 = str(int(parts[6]) - offset)
+        if parts[8] != '_':
+            new_parts = []
+            old_parts = parts[8].split('|')
+            for part in old_parts:
+                firstcolon = part.index(':')
+                target = part[:firstcolon]
+                new_target = target
+                if new_target != '0':
+                    new_target = str(int(target) - offset)
+                new_parts.append(new_target + part[firstcolon:])
+            part_8 = '|'.join(new_parts)
+        else:
+            part_8 = '_'
+        assert(part_0 and parts[1] and parts[3] and parts[4] and parts[5] and part_6 and parts[7] and part_8 and parts[9])
+        new_lines.append('\t'.join(
+            (part_0,
+             parts[1],
+             '_',
+             parts[3],
+             parts[4],
+             parts[5],
+             part_6,
+             parts[7],
+             part_8,
+             parts[9]
+            )))
+    return '\n'.join(new_lines)
 
 for filename in os.listdir(readdirpath):
     if not filename.startswith("Q"):# or '_part_' in filename:
--- scripts/list_ambiguous_analyses.py
+++ scripts/list_ambiguous_analyses.py
@@ -1,6 +1,7 @@
-import os
+import sys, os
 
-filenames = os.listdir('merged_conllu_from_webanno')
+rootdir = sys.argv[1]
+filenames = os.listdir(rootdir)
 
 def superset_relationship(p):
     a, b = p
@@ -11,27 +12,28 @@ def superset_relationship(p):
 forms = {}
 
 for f in filenames:
-    f_name = os.path.join('merged_conllu_from_webanno', f)
+    f_name = os.path.join(rootdir, f)
     for line in open(f_name):
         parts = line.strip().split('\t')
         if len(parts) != 10:
             continue
         lemma = parts[1]
         tag_parts = tuple(sorted(parts[5].split('|')))
-        if tag_parts == ('_',):
-            continue
+        # if tag_parts == ('_',):
+        #     continue
         if lemma not in forms:
             forms[lemma] = {tag_parts: 1}
             continue
         forms[lemma][tag_parts] = forms[lemma].get(tag_parts, 0) + 1
 for form in sorted(forms):
-    if len(forms[form].keys()) < 2:
+    # if len(forms[form].keys()) < 2:
+    #     continue
+    if len(forms[form].keys()) <= 2:
         continue
-    if len(forms[form].keys()) == 2:
-        if ('_',) in forms[form].keys():
-            continue
-        if superset_relationship(forms[form].keys()):
-            continue
+        # if ('_',) in forms[form].keys():
+        #     continue
+    # if superset_relationship(forms[form].keys()):
+    #     continue
     print(form)
     for analysis in forms[form]:
         print("  " + str(forms[form][analysis]) + "  " + '|'.join(analysis))
