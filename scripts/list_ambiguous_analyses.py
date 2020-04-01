19d6a30ab160a4a020752d7e063eeb2eb41f15b2
Sam Hardwick <sam.hardwick@iki.fi>
2020-04-01 14:11:49 +0300

    Ignore multipart lines in ambiguity lists

--- scripts/list_ambiguous_analyses.py
+++ scripts/list_ambiguous_analyses.py
@@ -10,9 +10,9 @@ def valid_filename(textname):
 
 def superset_relationship(p):
     a, b = p
-    a_parts = set(a)
-    b_parts = set(b)
-    return a_parts.issuperset(b_parts) or b_parts.issuperset(a_parts)
+    a_set = set(a)
+    b_set = set(b)
+    return a_set.issuperset(b_set) or b_set.issuperset(a_set)
 
 forms = {}
 
@@ -20,7 +20,7 @@ for f in filter(valid_filename, filenames):
     f_name = os.path.join(rootdir, f)
     for line in open(f_name):
         parts = line.strip().split('\t')
-        if len(parts) != 10:
+        if len(parts) != 10 or '-' in parts[0]:
             continue
         lemma = parts[1]
         tag_parts = tuple(sorted(parts[5].split('|')))
@@ -35,10 +35,10 @@ for form in sorted(forms):
     #     continue
     if len(forms[form].keys()) != 2:
         continue
+    elif superset_relationship(forms[form].keys()):
+        continue
         # if ('_',) in forms[form].keys():
         #     continue
-    if superset_relationship(forms[form].keys()):
-        continue
     print(form)
     for analysis in forms[form]:
         print("  " + str(forms[form][analysis]) + "  " + '|'.join(analysis))
